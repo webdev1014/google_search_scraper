23ea3a5b7b78028dd798169d8a7ce598fd9d6a00
webdev1014 <aleksagegicweb@gmail.com>
2021-01-19 21:48:49 +0200

    update puppeteer options

--- src/modules/scrape/service.ts
+++ src/modules/scrape/service.ts
@@ -15,8 +15,6 @@ export default class ScrapeService {
             '--ignore-certifcate-errors-spki-list',
             '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
             '--start-maximized',
-            '--no-sandbox',
-            '--disable-gpu',
         ],
         args: [
             '--no-sandbox',
